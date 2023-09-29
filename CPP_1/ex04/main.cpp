0a949a6fac1f47dd9a485dcbde8fb5039e97a8ba
isabel42 <106957731+isabel42@users.noreply.github.com>
2023-09-29 16:48:53 +0200

    ex 05

--- CPP_1/ex04/main.cpp
+++ CPP_1/ex04/main.cpp
@@ -6,27 +6,34 @@
 /*   By: itovar-n <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 13:29:23 by itovar-n          #+#    #+#             */
-/*   Updated: 2023/09/28 17:03:55 by itovar-n         ###   ########.fr       */
+/*   Updated: 2023/09/29 16:37:00 by itovar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
+void ft_replace(std::string *myText, std::string s1, std::string s2)
+{
+	std::size_t pos = (*myText).find(s1);
+
+	while (pos != std::string::npos)
+	{
+		(*myText).erase(pos, s1.length());
+		(*myText).insert(pos, s2);
+		pos = (*myText).find(s1);
+	}
+}
+
 int main(int argc, char** argv)
 {
-	if (argc != 2)
+	if (argc != 4)
 	{
 		std::cout << "Wrong number of arguments." << std::endl;
 		return (0);
 	}
-
-
-	// Create a text string, which is used to output the text file
 	std::string myText;
 	std::string myOutName = (std::string) argv[1] + ".replace";
-
-	// Read from the text file
 	std::ifstream MyReadFile(argv[1]);
 	if (!MyReadFile.is_open())
 	{
@@ -34,16 +41,18 @@ int main(int argc, char** argv)
 		return (-1);
 	}
 	std::ofstream MyOutFile(myOutName);
-
-	// Use a while loop together with the getline() function to read the file line by line
-	while (getline (MyReadFile, myText)) 
+	if (!MyOutFile.is_open())
 	{
-  		// Output the text from the file
+		std::cout << "File " << myOutName << "could not be created." << std::endl;
+		return (-1);
+	}
+	while (std::getline (MyReadFile, myText, '\0')) 
+	{
+		ft_replace(&myText, argv[2], argv[3]);
 		MyOutFile << myText;
 	}
+	MyReadFile.close();
+	MyOutFile.close();
 
-// Close the file
-MyReadFile.close();
- 
     return 0;
 }
---
+++ CPP_1/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itovar-n <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 16:39:07 by itovar-n          #+#    #+#             */
+/*   Updated: 2023/09/29 16:46:39 by itovar-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("XXX");
+	return (0);
+}
